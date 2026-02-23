3337916fff73a5002e4cc8d4431e71065c0433c9
BlackScull12 <87164998+BlackScull12@users.noreply.github.com>
2026-02-23 15:40:48 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+import authRoutes from "./routes/auth.js";
+import productRoutes from "./routes/products.js";
+import dropRoutes from "./routes/drop.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/drop", dropRoutes);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on ${process.env.PORT}`)
+);
