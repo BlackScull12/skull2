63577b6c32ee6903270a820eaf168fbb936f8377
BlackScull12 <87164998+BlackScull12@users.noreply.github.com>
2026-02-23 16:30:12 +0530

    Update Admin.js

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
-const AdminSchema = new mongoose.Schema({
-  password: String
-});
-
-export default mongoose.model("Admin", AdminSchema);
+export default mongoose.model(
+  "Admin",
+  new mongoose.Schema({
+    password: String
+  })
+);
