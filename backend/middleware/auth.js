44498a87843f8fb27605a13c1db6283dba8ccfb3
BlackScull12 <87164998+BlackScull12@users.noreply.github.com>
2026-02-23 16:30:32 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,13 +2,12 @@ import jwt from "jsonwebtoken";
 
 export default function (req, res, next) {
   const token = req.headers.authorization;
-  if (!token) return res.status(401).json({ msg: "No token" });
+  if (!token) return res.sendStatus(401);
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.admin = decoded;
+    req.admin = jwt.verify(token, process.env.JWT_SECRET);
     next();
   } catch {
-    res.status(401).json({ msg: "Invalid token" });
+    res.sendStatus(403);
   }
 }
