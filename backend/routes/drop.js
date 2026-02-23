c8160c2e380d7f195288e411017b1e15e8965cb9
BlackScull12 <87164998+BlackScull12@users.noreply.github.com>
2026-02-23 16:31:27 +0530

    Update drop.js

--- backend/routes/drop.js
+++ backend/routes/drop.js
@@ -4,15 +4,13 @@ import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const drop = await Drop.findOne();
-  res.json(drop);
+router.get("/", async (_, res) => {
+  res.json(await Drop.findOne());
 });
 
 router.post("/", auth, async (req, res) => {
   await Drop.deleteMany();
-  const drop = await Drop.create({ date: req.body.date });
-  res.json(drop);
+  res.json(await Drop.create({ date: req.body.date }));
 });
 
 export default router;
